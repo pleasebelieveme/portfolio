3ac957b6956c3ca1789d5be8d3e42810cfde7517
pleasebelieveme <cain1227@naver.com>
2026-01-02 13:16:46 +0900

    v1: 테일윈드 적용 및 확인

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <h1>홈 입니다</h1>
+    <h1 className="text-3xl font-bold underline">
+      홈입니다!
+    </h1>
     </Layout>
   )
 }
