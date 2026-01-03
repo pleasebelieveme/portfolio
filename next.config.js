7971a354bc42d496b06adba00dff9c5e686df5b2
pleasebelieveme <cain1227@naver.com>
2026-01-04 02:52:25 +0900

    v1: 프로젝트 화면 구현

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'www.notion.so',
+      'prod-files-secure.s3.us-west-2.amazonaws.com',
+      'images.unsplash.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
